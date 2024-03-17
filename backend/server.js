d381987a0fea34d979273eb0036398b558682079
Renek <jaes.ang.up@phinmaed.com>
2024-03-17 17:08:27 +0800

    database initialized. (mongodb) CRUD functionalities made. learned how to make models and schemas. learned how to connect the database. learned findById, findByIdAndUpdate, findByIdAndDelete, and deleteOne. learned how to put messages inside json and then get updated/deleted object.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware')
+const {errorHandler} = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+connectDB()
 
 const app = express();
 
@@ -11,5 +15,6 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/api/goals', require('../routes/goalRoutes'));
 app.use(errorHandler)
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
